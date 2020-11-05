c5d44e8a0ce38e8bb4b940d5f7c62b09ec73f98b
Michael <micjanic20@gmail.com>
2020-11-05 16:12:11 -0600

    set up todos for testing login

---
+++ src/AuthProvider.spec.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+describe("AuthProvider", () => {
+    it.todo("authed is false by default")
+})
+describe(".login", () => {
+    it.todo("sets authed to true")
+})
+describe(".logout", () => {
+    it.todo("sets authed to false")
+})
