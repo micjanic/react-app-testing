9504fbf16b8a29903851593fc215121a2325f481
Michael <micjanic20@gmail.com>
2020-11-05 16:41:34 -0600

    first test

--- src/AuthProvider.spec.js
+++ src/AuthProvider.spec.js
@@ -1,11 +1,26 @@
 import React from "react"
+import { render } from "@testing-library/react"
+import { AuthProvider, AuthContext } from "./AuthProvider"
 
 describe("AuthProvider", () => {
-    it.todo("authed is false by default")
-})
-describe(".login", () => {
-    it.todo("sets authed to true")
-})
-describe(".logout", () => {
-    it.todo("sets authed to false")
+    it("authed is false by default", () => {
+        const { getByText } = render(
+            <AuthProvider>
+                <AuthContext.Consumer>
+                    {(value) => (
+                        <span>Is logged in: {value.authed.toString()}</span>
+                    )}
+                </AuthContext.Consumer>
+            </AuthProvider>
+        )
+
+        expect(getByText("Is logged in: false")).toBeTruthy()
+    })
+
+    describe(".login", () => {
+        it.todo("sets authed to true")
+    })
+    describe(".logout", () => {
+        it.todo("sets authed to false")
+    })
 })
